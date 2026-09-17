8f2080ca62add36c1f10127da35e0e2933d0f036
kotovine <kotovine510@gmail.com>
2026-09-17 16:51:07 +0300

    KAN-5: Инициализация monorepo

---
+++ apps/web/src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const title = "URL Shortener";
+</script>
+
+<template>
+  <main>
+    <h1>{{ title }}</h1>
+    <p>Frontend-приложение готово к разработке.</p>
+  </main>
+</template>
---
+++ eslint.config.js
@@ -0,0 +1,27 @@
+import js from "@eslint/js";
+import prettierConfig from "eslint-config-prettier";
+import vue from "eslint-plugin-vue";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  {
+    ignores: ["**/dist/**", "**/node_modules/**", "**/coverage/**"],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...vue.configs["flat/recommended"],
+  {
+    files: ["**/*.{ts,tsx,vue}"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+      parserOptions: {
+        parser: tseslint.parser,
+      },
+    },
+  },
+  prettierConfig,
+);
